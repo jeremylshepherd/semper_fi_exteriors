4ed1d93e456386fda3697d0eb73780194ebd8be7
Jeremy L. Shepherd <jeremylshepherd@gmail.com>
2019-11-30 11:42:22 -0500

    Implemented gallery slider and dark mode

--- src/index.js
+++ src/index.js
@@ -12,7 +12,10 @@ const cta = _$(".cta-btn");
 const downContainer = _$(".down-container");
 const downButton = _$(".fa-angle-down");
 const sect1 = _$(".sect-1");
-const galleryImages = _$All(".gallery-img");
+const galleryImages = _$All(".gallery-slide");
+const galleryRightButton = _$(".gallery__btn--right");
+const galleryLeftButton = _$(".gallery__btn--left");
+const galleryContainer = _$(".gallery-container");
 const heroOptions = {
   rootMargin: "-360px 0px 0px 0px"
 };
@@ -21,6 +24,64 @@ const heroOptions = {
 if (!CSS.supports("backdrop-filter", "blur()")) {
   root.style.setProperty("--nav-bg", "rgba(255, 255, 255, 0.95)");
 }
+
+galleryImages[0].classList.add("current-slide");
+
+function setSlideWidths() {
+  let slideWidth = galleryImages[0].getBoundingClientRect().width;
+  galleryImages.forEach((img, i) => {
+    let current = img.style.left;
+    img.style.left = `${slideWidth * i}px`;
+  });
+}
+
+//Initialize Slide widths
+setSlideWidths();
+
+window.addEventListener("resize", () => {
+  console.log("window-resized");
+  setSlideWidths();
+  galleryContainer.style.transform = `translateX(-${
+    _$(".current-slide").style.left
+  })`;
+});
+
+galleryImages[0].classList.add("current-slide");
+
+function moveSlide(container, pos, current, pending) {
+  container.style.transform = `translateX(-${pos})`;
+  current.classList.remove("current-slide");
+  pending.classList.add("current-slide");
+}
+
+function animateRight() {
+  let current = _$(".current-slide");
+  let nextSlide = current.nextElementSibling;
+  if (!nextSlide) {
+    nextSlide = galleryContainer.children[0];
+  }
+  let amountToMove = nextSlide.style.left;
+  moveSlide(galleryContainer, amountToMove, current, nextSlide);
+}
+
+function animateLeft() {
+  let current = _$(".current-slide");
+  let previousSlide = current.previousElementSibling;
+  if (!previousSlide) {
+    previousSlide =
+      galleryContainer.children[galleryContainer.children.length - 1];
+  }
+  let amountToMove = previousSlide.style.left;
+  moveSlide(galleryContainer, amountToMove, current, previousSlide);
+}
+
+galleryRightButton.addEventListener("click", () => {
+  animateRight();
+});
+
+galleryLeftButton.addEventListener("click", () => {
+  animateLeft();
+});
 
 //To animate the navbar on scroll and the slogan animation
 const heroObserver = new IntersectionObserver((entries, heroObserver) => {
